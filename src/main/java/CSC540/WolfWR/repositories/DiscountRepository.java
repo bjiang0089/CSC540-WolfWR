8ed55b1929a1b735e56bae1115a7cf65c4d07b48
Brandon Jiang <bjiang0089@gmail.com>
2025-04-08 17:37:40 -0400

    Discount Repo changed to fit new key scheme

--- src/main/java/CSC540/WolfWR/repositories/DiscountRepository.java
+++ src/main/java/CSC540/WolfWR/repositories/DiscountRepository.java
@@ -3,5 +3,5 @@ package CSC540.WolfWR.repositories;
 import CSC540.WolfWR.models.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DiscountRepository extends JpaRepository<Discount, Long> {
+public interface DiscountRepository extends JpaRepository<Discount, Discount.DiscountID> {
 }
