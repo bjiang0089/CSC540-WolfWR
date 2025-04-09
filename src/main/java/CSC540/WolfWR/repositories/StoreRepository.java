43c4e041bdc959b90a37173fc9daf128027fa2cf
Brandon Jiang <bjiang9@ncsu.edu>
2025-04-09 19:08:56 -0400

    Added ID maker

--- src/main/java/CSC540/WolfWR/repositories/StoreRepository.java
+++ src/main/java/CSC540/WolfWR/repositories/StoreRepository.java
@@ -2,8 +2,14 @@ package CSC540.WolfWR.repositories;
 
 import CSC540.WolfWR.models.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    @Query(value =  "SELECT storeID FROM store", nativeQuery = true)
+    public List<Long> getIDs();
 }
