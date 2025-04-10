13240b12dab7bb38377e0b701cd05ad5a67c269a
Brandon Jiang <bjiang9@ncsu.edu>
2025-04-09 20:17:17 -0400

    Aggregating Billing and Customer actions into Manager

--- src/main/java/CSC540/WolfWR/views/ManagerView.java
+++ src/main/java/CSC540/WolfWR/views/ManagerView.java
@@ -1,21 +1,52 @@
 package CSC540.WolfWR.views;
 
+import CSC540.WolfWR.models.Member;
+import CSC540.WolfWR.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Scanner;
 
 @Component
 public class ManagerView {
 
-    public static void view(Scanner scan) {
+    @Autowired
+    private BillingStaffView billing;
+
+    @Autowired
+    private RegistrationView registration;
+
+    @Autowired
+    private WarehouseView warehouse;
+
+    @Autowired
+    private CustomerView customer;
+
+    @Autowired
+    private GlobalView global;
+
+    @Autowired
+    private MemberService memberServ;
+
+
+    public void view(Scanner scan) {
         String input = null;
         BillingStaffView billling = new BillingStaffView();
         WarehouseView warehouse = new WarehouseView();
         RegistrationView registration = new RegistrationView();
         while (true) {
 
-            System.out.println("Insert Message Body or " +
-                    "'back' to return to the previous page");
+            System.out.println("Select an action with the number provided:");
+
+            System.out.println("[0] Return to Home Page");
+            System.out.println("[1] Generate Bill for Supplier");
+            System.out.println("[2] Calculate Membership Rewards");
+            System.out.println("[3] Generate Sales Report (day, month, year)");
+            System.out.println("[4] Generate Sales Report (start - end)");
+            System.out.println("[5] View History");
+
+            System.out.print("> ");
             input = scan.nextLine().trim();
 
             if (input.equalsIgnoreCase("back")) {
@@ -30,10 +61,44 @@ public class ManagerView {
                 case "case 2":
                     billling.tabulateRewards(scan);
                     break;
+                case "3":
+                    billling.generateStoreSalesReport(scan);
+                    break;
+                case "4":
+                    billling.generateBoundStoreSalesReport(scan);
+                    break;
+                case "5":
+                    Member m = selectMember(scan);
+                    if (m == null) {
+                        break;
+                    }
+                    customer.viewHistory(m);
+                    break;
+                case "6":
+                    break;
                 default:
                     System.out.println("\nUnknown action\n");
             }
         }
 
+    }
+
+    public Member selectMember(Scanner scan) {
+        List<Member> members = memberServ.findAll();
+        for (int i = 0; i < members.size(); i++) {
+            Member m = members.get(i);
+            System.out.printf("[%d] %10s %10s. %s Member\n", i + 1, m.getFirstName(), m.getLastName(), m.getMembershipLevel());
+        }
+        System.out.print("> ");
+
+        String input = scan.nextLine().trim();
+        Member m = null;
+        try {
+            m = members.get( Integer.parseInt(input) );
+            return m;
+        } catch (Exception e) {
+            System.out.println("Invalid Selection");
+            return null;
+        }
     }
 }
