57119735aa39cee82bc2b4cae9d4b5f813e1a729
Bryan Rumsey <bmrumsey@gmail.com>
2025-04-09 22:20:24 -0400

    Added docs for the Store repository.

--- src/main/java/CSC540/WolfWR/repositories/StoreRepository.java
+++ src/main/java/CSC540/WolfWR/repositories/StoreRepository.java
@@ -4,6 +4,20 @@ import CSC540.WolfWR.models.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for accessing {@link Store} entities from the database.
+ * <p>
+ * This interface extends {@link JpaRepository}, providing methods for performing CRUD (Create, Read, Update, Delete)
+ * operations on {@link Store} entities. It facilitates interactions with the data layer for managing store information.
+ * </p>
+ * 
+ * <p>
+ * This repository is used for managing the data associated with stores, including details like store ID, address,
+ * contact information, and the store manager.
+ * </p>
+ * 
+ * @see Store
+ */
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 }
