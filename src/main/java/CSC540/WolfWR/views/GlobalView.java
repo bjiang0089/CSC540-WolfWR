52e38a94182efe552a432a29778e2b11aaf9d4ce
Bryan Rumsey <bmrumsey@gmail.com>
2025-04-09 22:55:17 -0400

    Added docs for the Global view.

--- src/main/java/CSC540/WolfWR/views/GlobalView.java
+++ src/main/java/CSC540/WolfWR/views/GlobalView.java
@@ -4,13 +4,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+/**
+ * The {@code GlobalView} class provides global views for the application. 
+ * This class includes methods to display the main view and allow for adding new locations.
+ * It is designed to be used as a component in a Spring application.
+ */
 @Component
 public class GlobalView {
 
+    /**
+     * Displays the global view for the application.
+     * This is a placeholder method for the main view functionality.
+     * 
+     * @param scan The {@link Scanner} object used to take user input.
+     */
     public void view(Scanner scan) {
 
     }
 
+    /**
+     * Allows for adding a new location (e.g., store or other relevant locations) 
+     * by taking input through the provided scanner.
+     * 
+     * @param scan The {@link Scanner} object used to take user input for adding a new location.
+     */
     public void addNewLocation( Scanner scan ) {
 
     }
