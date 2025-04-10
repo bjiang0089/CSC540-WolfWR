1820720b8d0119860b44380911f89f8ed4ee78eb
Bryan Rumsey <bmrumsey@gmail.com>
2025-04-09 22:12:34 -0400

    Added docs for the Discount repository.

--- src/main/java/CSC540/WolfWR/repositories/DiscountRepository.java
+++ src/main/java/CSC540/WolfWR/repositories/DiscountRepository.java
@@ -4,6 +4,17 @@ import CSC540.WolfWR.models.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for accessing `Discount` entities from the database.
+ * <p>
+ * This interface extends `JpaRepository`, providing methods for performing CRUD (Create, Read, Update, Delete)
+ * operations on `Discount` entities. The `Discount` entity is identified by a composite key defined by the `DiscountID` class.
+ * The repository is managed by Spring Data JPA, which automatically provides implementations for common database operations.
+ * </p>
+ * 
+ * @see Discount
+ * @see Discount.DiscountID
+ */
 @Repository
 public interface DiscountRepository extends JpaRepository<Discount, Discount.DiscountID> {
 }
