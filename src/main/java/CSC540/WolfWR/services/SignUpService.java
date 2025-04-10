dee7433f99b2e7d42ef2cc2bc08c4ab06cf91b48
Bryan Rumsey <bmrumsey@gmail.com>
2025-04-09 22:36:38 -0400

    Added docs for the Sign Up service.

--- src/main/java/CSC540/WolfWR/services/SignUpService.java
+++ src/main/java/CSC540/WolfWR/services/SignUpService.java
@@ -7,13 +7,34 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Service class responsible for handling operations related to {@link SignUp} entities.
+ * <p>
+ * This class extends the generic {@link Services} class and provides CRUD operations for
+ * managing {@link SignUp} entities. It interacts with the {@link SignUpRepository} to perform
+ * operations such as saving, finding, and deleting {@link SignUp} records from the database.
+ * </p>
+ * 
+ * @see SignUp
+ * @see SignUpRepository
+ * @see Services
+ */
 @Transactional
 @Component
 public class SignUpService extends  Services<SignUp, Long> {
 
+    /**
+     * The repository used for performing database operations related to {@link SignUp} entities.
+     */
     @Autowired
     private SignUpRepository repo;
 
+    /**
+     * Returns the repository used for performing database operations on {@link SignUp} entities.
+     * This method is implemented from the abstract {@link Services} class.
+     * 
+     * @return the {@link SignUpRepository} instance used to interact with the database.
+     */
     @Override
     protected JpaRepository<SignUp, Long> getRepo() {
         return this.repo;
