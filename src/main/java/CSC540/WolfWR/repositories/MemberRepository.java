1258bfcea2ea013c9a8e35aa4d3a29aab47fa9f0
Bryan Rumsey <bmrumsey@gmail.com>
2025-04-09 22:14:03 -0400

    Added docs for the Members repository.

--- src/main/java/CSC540/WolfWR/repositories/MemberRepository.java
+++ src/main/java/CSC540/WolfWR/repositories/MemberRepository.java
@@ -4,6 +4,16 @@ import CSC540.WolfWR.models.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for accessing `Member` entities from the database.
+ * <p>
+ * This interface extends `JpaRepository`, providing methods for performing CRUD (Create, Read, Update, Delete)
+ * operations on `Member` entities. The `Member` entity is identified by a `Long` type primary key.
+ * Spring Data JPA automatically provides implementations for common database operations.
+ * </p>
+ * 
+ * @see Member
+ */
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
